1d6ade85d8a7d4ba52c211a0d4aa2bf2dd62c3fc
Filip Troníček <filip.tronicek@seznam.cz>
2021-07-08 20:12:09 +0000

    Fix broken link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,7 +35,7 @@ module.exports = {
           items: [
             {
               label: 'Welcome',
-              to: '/docs/Welcome',
+              to: '/',
             },
           ],
         },
