74166a2c0d56f18d6fd850bf929b649a431b4f4c
Filip Troníček <filip.tronicek@seznam.cz>
2021-07-16 00:20:55 +0200

    Install Algolia search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,73 +1,77 @@
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'Interclip docs',
-  tagline: 'All about Interclip',
-  url: 'https://docs.interclip.app',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'aperta-principium', // Usually your GitHub org/user name.
-  projectName: 'Interclip', // Usually your repo name.
+  title: "Interclip docs",
+  tagline: "All about Interclip",
+  url: "https://docs.interclip.app",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "aperta-principium", // Usually your GitHub org/user name.
+  projectName: "Interclip", // Usually your repo name.
   themeConfig: {
+    algolia: {
+      apiKey: "YOUR_API_KEY",
+      indexName: "YOUR_INDEX_NAME",
+    },
     colorMode: {
       respectPrefersColorScheme: true,
     },
     announcementBar: {
-      id: 'beta_warning',
+      id: "beta_warning",
       content:
         'Welcome to our brand new docs! They are still in active development, so please, keep that in mind, and if you find anything wrong, <a target="_blank" rel="noreferrer noopener" href="https://github.com/interclip/docs/issues/new">submit an issue!</a>',
-      backgroundColor: '#fafbfc',
-      textColor: '#091E42',
+      backgroundColor: "#fafbfc",
+      textColor: "#091E42",
     },
     navbar: {
-      title: 'Interclip docs',
+      title: "Interclip docs",
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.png',
+        alt: "My Site Logo",
+        src: "img/logo.png",
       },
       items: [
         {
-          href: 'https://blog.trnck.dev',
-          label: 'Blog',
-          position: 'right',
+          href: "https://blog.trnck.dev",
+          label: "Blog",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Welcome',
-              to: '/',
+              label: "Welcome",
+              to: "/",
             },
           ],
         },
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'Twitter',
-              href: 'https://twitter.com/filiptronicek',
+              label: "Twitter",
+              href: "https://twitter.com/filiptronicek",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
               label: "Filip's Blog",
-              href: 'https://blog.trnck.dev',
+              href: "https://blog.trnck.dev",
             },
             {
-              label: 'GitHub',
-              href: 'https://github.com/aperta-principium/Interclip',
+              label: "GitHub",
+              href: "https://github.com/aperta-principium/Interclip",
             },
           ],
         },
@@ -82,16 +86,15 @@ module.exports = {
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          routeBasePath: '/',
-          editUrl:
-            'https://github.com/interclip/docs/edit/main/',
+          sidebarPath: require.resolve("./sidebars.js"),
+          routeBasePath: "/",
+          editUrl: "https://github.com/interclip/docs/edit/main/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
